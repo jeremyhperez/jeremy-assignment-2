afc17f54afd64b502a57574e819b937f7b419c17
jeremyhperez <jeremyhperez@gmail.com>
2024-05-02 14:21:43 -0700

    v2 changes

--- src/com/coderscampus/assignment2/Assignment2.java
+++ src/com/coderscampus/assignment2/Assignment2.java
@@ -5,6 +5,12 @@ public class Assignment2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// generate a random number
+		
+		// and collect user input
+		
+		// etc.
+		
 	}
 
 }
